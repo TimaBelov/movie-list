ab9c8b95eb0f611accfafd024bd45219be142cd7
Timofei Belov <timbe2017@yandex.ru>
2023-06-06 15:00:22 +0300

    add js code for del button

--- index.js
+++ index.js
@@ -3,41 +3,49 @@ const filmListNode = document.querySelector(".js-film-list");
 const filmInputNode = document.querySelector(".js-input-film");
 
 const FILM_WAS_SEEN_CLASSNAME = "film__checkbox__ON";
-
-filmArray = [];
+const BLACKOUT_ITEM = "film__item__blackout";
+const FILM_LIST_ITEM_DEL = "film__item__hidden";
 
 addFilmBtn.addEventListener("click", function () {
   if (filmInputNode.value != "") {
-    filmArray.push(filmInputNode.value);
-    renderFilmList();
+    renderFilmList(filmInputNode.value);
+    filmInputNode.value = "";
   }
 });
 
-function renderFilmList() {
+function renderFilmList(filmName) {
   filmHTML = "";
-  filmArray.forEach((item) => {
-    filmHTML += `<li class="film__item">
-    <div class="film__info">
-    <button class="film__checkbox"></button>
-    ${item}
-    </div>
-    <button class="film__del"></button>
-    </li>`;
-  });
-  filmListNode.innerHTML = filmHTML;
+  filmHTML += `<li class="film__item">
+  <div class="film__info">
+  <button class="film__checkbox"></button>
+  ${filmName}
+  </div>
+  <button class="film__del"></button>
+  </li>`;
+  filmListNode.innerHTML = filmHTML + filmListNode.innerHTML;
 }
 
 filmListNode.addEventListener("click", function (event) {
   let target = event.target;
   if (target.classList.contains("film__checkbox")) {
     filmWasSeen(target);
+  } else if (target.classList.contains("film__del")) {
+    filmDel(target);
   }
 });
 
 function filmWasSeen(target) {
+  let li = target.closest("li");
   if (target.classList.contains(FILM_WAS_SEEN_CLASSNAME)) {
     target.classList.remove(FILM_WAS_SEEN_CLASSNAME);
+    li.classList.remove(BLACKOUT_ITEM);
   } else {
     target.classList.add(FILM_WAS_SEEN_CLASSNAME);
+    li.classList.add(BLACKOUT_ITEM);
   }
+}
+
+function filmDel(target) {
+  let li = target.closest("li");
+  li.classList.add(FILM_LIST_ITEM_DEL);
 }
